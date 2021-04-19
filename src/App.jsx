13fe9f3893280a385f1b3ca4a6bbc14531150244
arunbhalli <tomarbhalli.com>
2021-04-19 08:43:03 +0200

    add modified weeklyweather

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import { Container, Header, Segment, Grid, Icon } from "semantic-ui-react";
 import Weeklyweather from "./components/Weeklyweather.jsx";
-import Hourlyweather from './components/Hourlyweather.jsx'
+import Hourlyweather from "./components/Hourlyweather.jsx";
 
 class App extends Component {
   state = {
@@ -11,7 +11,7 @@ class App extends Component {
     dailyWeather: {},
     hourlyWeather: {},
   };
-  
+
   getGeolocation = new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(resolve, reject);
   });
@@ -38,44 +38,58 @@ class App extends Component {
       timezone:
         locationResponse.data.results[0].annotations.timezone.short_name,
     };
-    this.setState({ 
-    location: weatherInfo,
-    dailyWeather: weatherResponse.data.daily,
-    hourlyWeather: weatherResponse.data.hourly,
-    geolocation: geoloc.coords });
+    this.setState({
+      location: weatherInfo,
+      dailyWeather: weatherResponse.data.daily,
+      hourlyWeather: weatherResponse.data.hourly,
+      geolocation: geoloc.coords,
+    });
   }
 
   render() {
     const { dailyWeather, hourlyWeather } = this.state;
-    const{city, temp, weather, timezone} = this.state.location;
+    const { city, temp, weather, timezone } = this.state.location;
     return (
       <>
         <Container data-cy="weather-display" fluid>
-          <Header as="h1" textAlign="center" style_id="head" >
-            <Header.Content data-cy="location"><Icon size="large" name="building outline"/>
+          <Header as="h1" textAlign="center" id="head">
+            <Header.Content data-cy="location">
+              <Icon size="large" name="building outline" />
               {city}
             </Header.Content>
           </Header>
-          <Segment float id="opaci" >
+          <Segment float id="opaci">
             <Grid verticalAlign="middle" columns={4} centered textAlign="left">
               <Grid.Row>
                 <Grid.Column>
-                  <Header textAlign="center" data-cy="temp"><Icon size="large" name="thermometer full"/>{temp}°C</Header>
+                  <Header textAlign="center" data-cy="temp">
+                    <Icon size="large" name="thermometer full" />
+                    {temp}°C
+                  </Header>
                 </Grid.Column>
                 <Grid.Column>
-                  <Header textAlign="center" data-cy="today" >Today</Header>
+                  <Header textAlign="center" data-cy="today">
+                    Today
+                  </Header>
                   <br />
-                  <Header textAlign="center" data-cy="timezone" ><Icon size="large" name="world"/>Time Zone: {timezone}</Header>
+                  <Header textAlign="center" data-cy="timezone">
+                    <Icon size="large" name="world" />
+                    Time Zone: {timezone}
+                  </Header>
                 </Grid.Column>
                 <Grid.Column>
-                  <Header textAlign="center" data-cy="weather-type"> <Icon name="cloudversify" />{weather}</Header>
+                  <Header textAlign="center" data-cy="weather-type">
+                    {" "}
+                    <Icon name="cloudversify" />
+                    {weather}
+                  </Header>
                 </Grid.Column>
               </Grid.Row>
             </Grid>
           </Segment>
           <Container>
-              <Weeklyweather dailyWeather={dailyWeather} />
-              <Hourlyweather hourlyWeather={hourlyWeather} />
+            <Weeklyweather dailyWeather={dailyWeather} />
+            <Hourlyweather hourlyWeather={hourlyWeather} />
           </Container>
         </Container>
       </>
--- src/components/Weeklyweather.jsx
+++ src/components/Weeklyweather.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { Grid,Header} from "semantic-ui-react";
+import { Grid, Header } from "semantic-ui-react";
 
 // weekend challenge
 const Weeklyweather = ({ dailyWeather }) => {
-
   const forecastList = [];
   for (let i = 1; i < dailyWeather.length; i++) {
     const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -12,15 +11,21 @@ const Weeklyweather = ({ dailyWeather }) => {
         <Header.Content data-cy="week-day" className="days">
           {days[new Date(dailyWeather[i].dt * 1000).getDay()]}
         </Header.Content>
-        <Header.Content data-cy="daily-weather">{dailyWeather[i].weather[0].description}</Header.Content>
-        <Header.Content data-cy="daily-temp">{dailyWeather[i].temp.day}°C</Header.Content>
+        <Header.Content data-cy="daily-weather">
+          {dailyWeather[i].weather[0].description}
+        </Header.Content>
+        <Header.Content data-cy="daily-temp">
+          {dailyWeather[i].temp.day}°C
+        </Header.Content>
       </Grid.Column>
     );
   }
 
   return (
-    <Grid columns={7} celled id="hou" >
-      <Grid.Row id="opaci" color="black" inverted data-cy="seven-days-list" >{forecastList}</Grid.Row>
+    <Grid columns={7} celled id="hou">
+      <Grid.Row id="opaci" color="black" inverted data-cy="seven-days-list">
+        {forecastList}
+      </Grid.Row>
     </Grid>
   );
 };
